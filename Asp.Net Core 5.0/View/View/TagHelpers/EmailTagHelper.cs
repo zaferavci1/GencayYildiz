493677ae4714f902b3ffa18119f5412ab3b77d08
Zafer Avcı <zaferavci89@gmail.com>
2023-04-27 20:52:30 +0300

    Custom TagHelper

---
+++ Asp.Net Core 5.0/View/View/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace View.TagHelpers
+{
+    [HtmlTargetElement("mail1")]// TagHelper'ın sınıf isminden farklı olması için kullanılıyor.
+    public class EmailTagHelper : TagHelper//Bir sınıfın TagHelper olabilmesi için TagHelper sınıfından türemesi gereklidir.
+    {
+        public string Mail { get; set; }
+        public string Display { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+            //TagHelper'ın operasyonunun işlevini gerçekleşmesi için Proccess metodunun override edilmesi gereklidir.
+        {
+            //context parametresi bize html dosyasında verdiğimiz değerleri getirecek. İlgili TagHelper'ı getirebilmekte. Attributelar getiriyor
+            //output ilgili attribute'ların çıktısını bize veriyor.
+            output.TagName = "a";
+            output.Attributes.Add("href", $"mailto:{Mail}");
+            output.Content.Append(Display);
+        }
+    }
+}
