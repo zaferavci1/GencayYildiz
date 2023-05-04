f81acfd84ef183c9ff1fcadcfc63555db50bfd04
Zafer Avcı <zaferavci89@gmail.com>
2023-05-04 12:35:49 +0300

    Model Binding mekanizmasını nasıl çalışıtığını gördük

---
+++ Asp.Net Core 5.0/ModelBinding/ModelBinding/Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelBinding.Models;
+
+namespace ModelBinding.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult GetProducts()
+        {
+
+            return View();
+        }
+        public IActionResult CreateProduct()
+        {
+            /*
+             * Bir action metot varsayılan (Default) olarak HttpGet türündendir
+             * Bu controller Get isteiğinde tetiklenecek 
+             * Biz Controller dan viewa bir nesne gönderdiğğimiz zaman Binding mekanizması yine çalışacak. 
+             * Binding mekanizması iki yönlü olarak da çalışabiliyor. 
+             * Biz controllerdan gönderdiğimiz nesne Post edilidiğinde yeni bir nesen oluşturulmayacaktır. Bizim gönderdiğimiz nesne üzerine yazılacaktır. Gönderdiğimiz nesen post controllerına geri gönderilecektir.
+             */
+            Product product = new Product()
+            {
+                Name = "Test",
+                Quantity=0
+            };
+            return View(product);
+        }
+
+        [HttpPost]
+        //public IActionResult CreateProduct(string txtProductName, string txtQuantity)
+        public IActionResult CreateProduct(Product product)
+        {
+            // Bu controller Post isteiğinde tetiklenecek. Mimamri bunu bilecek
+            // Request neticesinde gelen dataların hepsi ait olduğu contollerın parametlerinde yakalanmaktadır
+            // Form içerisindeki input nesneleri post edildiğinde bu nesnelere karşılık gelen propertyleri karşılayan bir nesne ile otomatik olarak bind edilirler
+            return View();
+        }
+    }
+}
