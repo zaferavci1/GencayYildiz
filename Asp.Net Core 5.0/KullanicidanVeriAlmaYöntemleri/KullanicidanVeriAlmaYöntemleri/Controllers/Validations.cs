120873cc5b22305ced668a4f05350b29a2b016db
Zafer Avcı <zaferavci89@gmail.com>
2023-05-31 14:19:42 +0300

    client tabanlı validations

---
+++ Asp.Net Core 5.0/KullanicidanVeriAlmaYöntemleri/KullanicidanVeriAlmaYöntemleri/Controllers/Validations.cs
@@ -0,0 +1,56 @@
+﻿using KullanicidanVeriAlmaYöntemleri.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace KullanicidanVeriAlmaYöntemleri.Controllers
+{
+    public class Validations : Controller
+    {
+        public IActionResult Validation()
+        {
+            /*      Validation 
+             * Bir degerin içinde bulunduğu sartlara uygun olması durumudur.
+             * Belirlenen koşullara ve amaca uygun olup olmama durumunun kontrol edilmesidir.
+             * Bu konrole göre verinin işleme tabi edilmesi durumudur.
+             * 
+             */
+
+            /* Validation İşlemi Server tarafında kontrol ediliyor ise
+             * Biz buna Server Side Validation deriz.
+             * Client tarafında ise Client Side Validation denir. Client'a fazla güvenmeyiz Server tarafında da doğrulama da yaparız.
+             * Validation Client ve Server tarafında uygulanmalıdır.
+             */
+            return View();
+        }
+
+        public IActionResult CreateProductValidations()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateProductValidations(Product model)
+        {
+            /*İNANAILMAZ DERECEDE KÖTÜ KOT/MALİYET FAZLA/YÖNETİLEBİLİRLİK AZ
+            if (string.IsNullOrEmpty(model.Name) && model.Name.Length<100 )
+            {
+
+            }
+            */
+            // ModelState: MVC'de bir type'ın data annotationsların durumunu kontrol eden ve geriye sonuç deönen bir propertydir.
+            if (!ModelState.IsValid)
+            {
+                //doğrulanmaz ise 
+                //loglanma
+                //kullanıcı bilgilendirme
+                //ViewBag.HataMesaj = ModelState.Values.FirstOrDefault(x => x.ValidationState == Microsoft.AspNetCore.Mvc.ModelBinding.ModelValidationState.Invalid).Errors[0].ErrorMessage;
+                var messages =ModelState.ToList();
+
+                return View(model);// Modeli tekrar gönderdiğimiz için gidecek meseal name isimlli alana spanın içerisinde bu hata mesajını yazacak
+            }
+
+            //işlem/operasyon/algoritma burada tabi tutulur.
+            return View();
+        }
+    }
+}
--- Asp.Net Core 5.0/KullanicidanVeriAlmaYöntemleri/KullanicidanVeriAlmaYöntemleri/Models/Product.cs
+++ Asp.Net Core 5.0/KullanicidanVeriAlmaYöntemleri/KullanicidanVeriAlmaYöntemleri/Models/Product.cs
@@ -1,8 +1,15 @@
-﻿namespace KullanicidanVeriAlmaYöntemleri.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KullanicidanVeriAlmaYöntemleri.Models
 {
     public class Product
     {
+        [Required(ErrorMessage ="Lütfen Product Name'i giriniz.")]
+        [StringLength(100,ErrorMessage ="Lütfen product namei 100 karakter giriniz.")]
         public string Name { get; set; }
         public int Quantity { get; set; }
+        [EmailAddress(ErrorMessage ="Lütfen doğru bir email adresi giriniz.")]
+        public string Email { get; set; }
+
     }
 }
